1b3de270e51f970eba36d70855eeb1ee33e87f24
MattyOL <mattyoleary1997@gmail.com>
2023-06-27 12:50:36 +0000

    Heroku set up

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+import os
+import dj_database_url
+
 if  os.path.exists("env.py"):
     import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,15 +123,25 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
+     
+# DATABASES = {
+ #    'default': dj_database_url.parse('postgres://dgkwmcno:L8v-LKJgVRiLbCW9x4aRi0-SfU5x0R13@tyke.db.elephantsql.com/dgkwmcno')
+# }
+    
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
